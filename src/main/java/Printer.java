6a60bba1c1f3e5ac8fc8fd41ece53f4b7d1a8073
Mark Richardson <mark.richardson4@gmail.com>
2021-01-25 19:17:59 +0000

    first extension test written for printer, method then created to pass the test

---
+++ src/main/java/Printer.java
@@ -0,0 +1,13 @@
+public class Printer {
+
+    private int paperLevel;
+
+    public Printer(int paperLevel){
+        this.paperLevel = paperLevel;
+    }
+
+    public int getPaperLevel(){
+        return paperLevel;
+    }
+
+}
