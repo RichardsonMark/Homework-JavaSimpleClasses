7d0f68d818f71688da3e4719d1cf11fab2506011
Mark Richardson <mark.richardson4@gmail.com>
2021-01-25 17:23:34 +0000

    first test written for calclator (add) then method created to pass the test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void add(){
+        assertEquals(4, calculator.add(2, 2));
+    }
+
 }
